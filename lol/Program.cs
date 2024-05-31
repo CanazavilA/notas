5f7d09175ce74db854d30dc32ae2b2164c06c973
CanazavilA <168607080+CanazavilA@users.noreply.github.com>
2024-05-30 21:22:40 -0300

    Add files via upload

---
+++ lol/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Numerics;
+class miClase {
+
+    /*Hacer un programa que permita cargar por programa 6 notas de un alumno en un vector
+      luego de cargar todas las notas. debera recorrer el vector de dichas notas
+      para mostrar la nota mas alta, la nota mas baja y el promedio general.
+    */
+    static void Main (){
+
+        //declaracion del vector
+            int[] vector = new int[6];
+
+            int mayor = 0;
+            int menor = 0;
+            int acumulador = 0;
+
+        //llenado del vector
+            for (int i=0 ; i<=5 ; i++){
+                
+                Console.Write("Ingrese la nota ({0}):", i+1);
+                vector[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        menor = vector[0];
+
+        foreach (var elemento in vector) {
+
+                if (elemento > mayor){
+                    mayor=elemento;
+                }
+
+                if (elemento < menor){
+                    menor=elemento;
+                }
+
+                acumulador = acumulador + elemento;
+            
+
+            }   
+                Console.WriteLine("mayor: {0}",mayor);
+                Console.WriteLine("menor: {0}",menor);
+                Console.WriteLine("suma: {0}",acumulador);
+                Console.WriteLine("promedio: {0}",acumulador/6);
+        }
+    }
---
+++ lol/a.cs
@@ -0,0 +1,67 @@
+//hacer un programa que permita ingresar por teclado tres edades, al finalizar mostrar los datos ingresados
+/*
+int cont=0;
+
+while(cont<3)
+{
+    System.Console.WriteLine("Ingrese una edad");
+   int edad=int.Parse(Console.ReadLine());
+
+    System.Console.WriteLine("Las edades ingresadas fueron " + edad);
+    cont++;
+}
+string[] edad=new string[3];
+
+edad[0] = "1";
+edad[1] = "10";
+edad[2] = "23";
+
+Console.WriteLine(edad[0]);
+
+int tamanio =7;
+int[]edad=new int[4];
+for(int i=0;i<tamanio;i++)
+{
+    edad[i]=i+1;
+}
+for(int i=0;i<tamanio;i++)
+{
+    System.Console.WriteLine(edad[i]);
+}
+*/
+
+/*
+using System;
+class miClase {
+
+    /*Hacer un programa que permita cargar por programa 6 notas de un alumno en un vector
+      luego de cargar todas las notas. debera recorrer el vector de dichas notas
+      para mostrar la nota mas alta, la nota mas baja y el promedio general.
+    */
+    /*
+    static void Main (){
+
+        //declaracion del vector
+            int[] vector = new int[6];
+      
+        //llenado del vector
+            for (int i=0 ; i<=5 ; i++){
+            
+                vector[i]= i+1;
+            }
+
+        //Muestra los datos
+        /*
+        for (int i=0 ; i<=4 ; i++){
+
+            Console.WriteLine(vector[i]);
+        }
+        
+
+        foreach (int elemento in vector) {
+            
+                Console.WriteLine(elemento);
+            }
+        }
+    }
+*/
