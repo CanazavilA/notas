f4476344f8e9f5b8d8c107db6f3f99e6f36279bc
CanazavilA <168607080+CanazavilA@users.noreply.github.com>
2024-05-30 21:56:17 -0300

    Add files via upload

---
+++ pepe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+namespace miLibreria{
+    
+  class miClase{
+
+    static void Main(){
+
+        int contador = 1;
+
+        int[,] matriz = new int [3,4];
+            for(int j=0; j< 3; j++){
+            for(int i=0; i<4; i++) {
+              
+                matriz[j,i]= contador;
+
+                contador++; 
+
+                 }
+               
+            }
+                for(int j=0; j<3; j++){
+                    for (int i=0; i<4; i++){
+                        Console.WriteLine("Matriz({0},{1}): {2}",j,i,matriz[j,i]);
+                    }
+                }
+   
+    }
+
+  }
+}
